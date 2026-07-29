ef63631d2c7ad7cb19c7bce3de39c7e65784e076
zn4qnkvdxr-web <zn4qnkvdxr@privaterelay.appleid.com>
2026-07-29 10:11:02 +0200

    Alignement RCT - PROD

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  * ⚠ DÉPLOIEMENT : incrémenter VERSION à CHAQUE déploiement (voir DEPLOIEMENT.md).
  *   C'est ce numéro qui invalide l'ancien cache de coquille chez les visiteurs.
  */
-const VERSION = 'cv-ia-v74';
+const VERSION = 'cv-ia-v75';
 
 const SHELL = [
   '/',
